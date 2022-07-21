4738935c3831e5849fe9da31de9adcf14035c677
VadimKachuryn <vadimkacurin71@gmail.com>
2022-07-21 17:45:28 +0300

    add login

--- src/classes/DBUpdater.php
+++ src/classes/DBUpdater.php
@@ -5,26 +5,45 @@ class DBUpdater
     // key fields for DB file
     private $login;
     private $password;
-    //
-    // data by input key
-    private $data;
+    // DB file name;
+    private $fileName;
+    private $jsonArray;
 
 
-    public function __construct(string $iLogin, string $iPassword)
+    public function __construct(string $iLogin, string $iPassword, string $inFileName)
     {
         $this->login = $iLogin;
         $this->password = $iPassword;
+        $this->fileName = $inFileName;
+        $this->readFile();
     }
 
-    public function isRecordExist() {
-
+    public function checkIsRecordExist()
+    {
+        foreach ($this->jsonArray as $key) {
+            if ((strval($key->login) == strval($this->login)) && (strval($key->password) == strval($this->password)))
+                return true;
+            else
+                return false;
+        }
     }
 
-    private function checkMode(String $iMode) {
+    private function checkMode(string $iMode)
+    {
         if ($iMode != ADD_RECORD || $iMode != MODIFY_RECORD || $iMode != DELETE_RECORD) {
             return array("Неверный режим работы с данными");
         } else {
             return array("");
+        }
+    }
+
+    private function readFile()
+    {
+        if (file_exists($this->fileName)) {
+            $json = file_get_contents($this->fileName);
+            $this->jsonArray = json_decode($json, true);
+        } else {
+            $this->jsonArray = [];
         }
     }
 
--- src/forms/login.php
+++ src/forms/login.php
@@ -1,36 +1,40 @@
 <?php
 require '../classes/Validator.php';
+require '../classes/DBUpdater.php';
 require '../settings/constants.php';
-    $data = $_POST;
-    if( isset($data['do_login'])) {
+$fileName = "LoginUsers.json";
+$data = $_POST;
+if (isset($data['do_login'])) {
 
-        $loginValidator = new Validator($data['login'], $data['password']);
-        $error = $loginValidator->validateLogin();
-        if (!empty($error)) {
-            echo '<div style="color: red;">' . array_shift($error) . '</div><hr>';
-        }
-        $error = $loginValidator->validatePassword();
-        if (!empty($error)) {
-            echo '<div style="color: red;">' . array_shift($error) . '</div><hr>';
-        }
+    $loginValidator = new Validator($data['login'], $data['password']);
+    $error = $loginValidator->validateLogin();
+        echo '<div style="color: red;">' . array_shift($error) . '</div><hr>';
 
-        $dbUpdater = new DBUpdater($data['login'], $data['password']);
+    $error = $loginValidator->validatePassword();
+    if (!empty($error))
+        echo '<div style="color: red;">' . array_shift($error) . '</div><hr>';
+
+    $dbData = new DBUpdater($data['login'], $data['password'], $fileName);
+    if (!$dbData->checkIsRecordExist()) {
+        $error = array("Пользователь с таким логином не найден!");
+        echo '<div style="color: red;">' . array_shift($error) . '</div><hr>';
     }
+}
 ?>
 
 <form action="login.php" method="POST">
     <p>
-        <p><strong>Логин</strong>:</p>
-        <input type="text" name="login" value="<?php echo @$data['login']; ?>">
-    </p> 
-    
+    <p><strong>Логин</strong>:</p>
+    <input type="text" name="login" value="<?php echo @$data['login']; ?>">
+    </p>
+
     <p>
-        <p><strong>Пароль</strong>:</p>
-        <input type="password" name="password" value="<?php echo @$data['password']; ?>">
-    </p> 
-    
+    <p><strong>Пароль</strong>:</p>
+    <input type="password" name="password" value="<?php echo @$data['password']; ?>">
+    </p>
+
     <p>
-        <button type="submit" name= "do_login">Войти</button>
-    </p>    
+        <button type="submit" name="do_login">Войти</button>
+    </p>
 
 </form>
